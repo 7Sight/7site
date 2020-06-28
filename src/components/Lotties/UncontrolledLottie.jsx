a844d5a0f78d78d3526ccdc04d598be97ece03e0
jonas97salatiel <jonas97salatiel@gmail.com>
2020-06-28 00:50:08 -0300

    Incluindo nova animação em serviços

---
+++ src/components/Lotties/UncontrolledLottie.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import Lottie from 'react-lottie'
+import animationData from '../../lotties/lf30_editor_4hpwS0.json'
+
+class UncontrolledLottie extends Component {
+
+
+  render(){
+
+    const defaultOptions = {
+      
+      loop: true,
+      autoplay: true,
+      animationData: animationData,
+      rendererSettings: {
+        preserveAspectRatio: 'slice'
+      }
+    };
+
+    return(
+      <div>
+        <Lottie options={defaultOptions}
+              height={350}
+              width={350}
+              resizeMode="contain"
+              autoZise
+        />
+      </div>
+    )
+  }
+}
+
+export default UncontrolledLottie
--- src/components/Servicos/index.js
+++ src/components/Servicos/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Lutties from '../Lotties/UncontrolledLottie'
 import './style.css'
 
 class Servicos extends Component {
@@ -10,31 +11,9 @@ class Servicos extends Component {
                <div className="title__servicos">Servicos</div>
                <div className="traco"></div>
                <div className="servicos__itens">
-                  <div className={this.props.className}>
-                     <div className="website__nav">
-                        <div className="website__logo"></div>
-                        
-                        <div className="website__menu">
-                           <div className="website__menu__itens"></div>
-                           <div className="website__menu__itens"></div>
-                           <div className="website__menu__itens"></div>
-                        </div>
-                     </div>
-                     <div className="website__section">
-                        <div className="website__section__img"></div>
-                        <div className="website__section__txt">
-                           <div className="website__section__txt__item"></div>
-                           <div className="website__section__txt__item"></div>
-                           <div className="website__section__txt__item"></div>
-                        </div>
-                     </div>
-                     <div className="website__section1">
-                           <div className="website__section__txt__item1"></div>
-                           <div className="website__section__txt__item1"></div>
-                           <div className="website__section__txt__item1"></div>
-                     </div>
+                  
+                  <Lutties />
 
-                  </div>  
                   <div className="servicos__desc">
                      <h1 className="servicos__desc__title">Desenvolvimento de sites</h1>
                      <p className="servicos__desc__text">Desenvolvimento de sites responsivos com sistemas gerenciadores de conteúdo (CMS).</p>
