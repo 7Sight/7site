d8f37dbd09bc3fa99e4d0ee61f2bdf70274adc0f
jonas97salatiel <jonas97salatiel@gmail.com>
2020-09-10 00:39:50 -0300

    Teste de build

---
+++ build/precache-manifest.e8d26f96519834e721497627c7c47991.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e3d2b6af06c2dd1927e63ac8100ec97",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78f1e3f094375b65d308",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "5a63958e779a12d28175",
+    "url": "/static/css/main.a73c7bd8.chunk.css"
+  },
+  {
+    "revision": "78f1e3f094375b65d308",
+    "url": "/static/js/2.a3fc0abe.chunk.js"
+  },
+  {
+    "revision": "6485e28f1bc40c1e743bb4de5f286310",
+    "url": "/static/js/2.a3fc0abe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a63958e779a12d28175",
+    "url": "/static/js/main.94c1fdd7.chunk.js"
+  },
+  {
+    "revision": "f4a0c7820fdab4d6ca50",
+    "url": "/static/js/runtime-main.cf66e465.js"
+  },
+  {
+    "revision": "ade91f473255991f410f61857696434b",
+    "url": "/static/media/Montserrat-Bold.ade91f47.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "e1996192b98a516646ff9a8c0c0ca90c",
+    "url": "/static/media/Oswald-Regular.e1996192.ttf"
+  },
+  {
+    "revision": "4a404ca594f699b69485c1a5324f14e8",
+    "url": "/static/media/aline.4a404ca5.jpg"
+  },
+  {
+    "revision": "d1f758f319f082d16bda4e345df396f5",
+    "url": "/static/media/carlos.d1f758f3.jpg"
+  },
+  {
+    "revision": "c66f7a1b88f341f7577cab582851628d",
+    "url": "/static/media/iconinstagran.c66f7a1b.svg"
+  },
+  {
+    "revision": "46b4582d6c254dcd56f977854536a7b6",
+    "url": "/static/media/iconlinkedin.46b4582d.svg"
+  },
+  {
+    "revision": "bb5b046fa4dc854e30cf2afe09a61b63",
+    "url": "/static/media/image.bb5b046f.jpg"
+  },
+  {
+    "revision": "1624a552e27eb7a97d57893f7eed8ac5",
+    "url": "/static/media/jess.1624a552.jpg"
+  },
+  {
+    "revision": "5f09ae59185358cd38663420df2790fc",
+    "url": "/static/media/jonas.5f09ae59.jpg"
+  },
+  {
+    "revision": "ee2017875c5e105138eebef63b7fe96e",
+    "url": "/static/media/logo.ee201787.png"
+  },
+  {
+    "revision": "d0ff33f87bc1fba33f77722c5f02e01c",
+    "url": "/static/media/vitor.d0ff33f8.jpg"
+  }
+]);
